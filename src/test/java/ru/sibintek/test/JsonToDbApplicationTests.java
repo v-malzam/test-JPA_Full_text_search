afe8707872489bc804896430d3a8069e4623e3e6
Vladimir Malzam <v.malzam@gmail.com>
2020-11-10 20:23:53 +0300

    added standard files structure

--- src/test/java/ru/sibintek/test/JsonToDbApplicationTests.java
+++ src/test/java/ru/sibintek/test/JsonToDbApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Test4JsonToDbApplicationTests {
+class JsonToDbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
