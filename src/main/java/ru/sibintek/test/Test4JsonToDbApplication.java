6e596665ee98ca1c6bb1106af46164f21905b287
Vladimir Malzam <v.malzam@gmail.com>
2020-11-10 15:45:59 +0300

    init commit

---
+++ src/main/java/ru/sibintek/test/Test4JsonToDbApplication.java
@@ -0,0 +1,13 @@
+package ru.sibintek.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test4JsonToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test4JsonToDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sibintek/test/Test4JsonToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sibintek.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test4JsonToDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
